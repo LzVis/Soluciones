51d5484386e8c373974e739b04853c0e2b9ed2ac
vss <maxi.valdes.v@gmail.com>
2024-04-23 21:59:50 -0400

    modificados:     Ej1.py

--- Ej1.py
+++ Ej1.py
@@ -1,6 +1,5 @@
 #importamos la funcion sqrt de la libreria de math
 from math import sqrt
-sqrt(100)
 
 #Solicitamos entradas al usuario y las almacenamos en sus respectivas variables
 #Entradas eje x
