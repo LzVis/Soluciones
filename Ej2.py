a57a2d8d25aee6f1c853b1cd4ec5b9887131f492
vss <maxi.valdes.v@gmail.com>
2024-04-27 14:50:35 -0400

    modificados:     Ej2.py nuevos archivos: Ej3.py

--- Ej2.py
+++ Ej2.py
@@ -3,3 +3,15 @@ from datetime import datetime
 HoraActual = datetime.now().hour
 MinutoActual = datetime.now().minute
 print(f"la hora es {HoraActual}:{MinutoActual}")
+
+#Solicitamos al usuario que ingrese su nombre
+nombre = input("Ingrese su nombre ") #Recordar que con input los datos ingresados son del tipo texto
+
+#Ahora saludamos al usuario y le indicamos la hora
+#Recordar que en python se hacen las salidas con print y asi estaremos imprimiendo unos datos de salida
+
+#Aca tendriamos 4 formas para mostrar el mismo texto, preferentemente es mas sencilla la primera.
+print(f"Bienvenid@ {nombre}, son las {HoraActual}:{MinutoActual}")
+print("Bienvenid@ {0}, son las {1}:{2}".format(nombre,HoraActual,MinutoActual))
+print("Bienvenid@ "+str(nombre)+" son las "+str(HoraActual)+":"+str(MinutoActual))
+print("Bienvenid@",nombre,"son las ",HoraActual,":",MinutoActual)
---
+++ Ej3.py
@@ -0,0 +1,15 @@
+tiempo = float(input("ingrese el tiempo en segundos "))
+DatoACalcular = input("Escriba el dato que desee calcular. \naltura \nvelocidad final\n")
+if DatoACalcular=="velocidad final": #Mediante la sentencia if le preguntamos si la declaracion es True y en caso de ser True, ejecutara lo que esta indexado 
+    velocidadFinal = 9.81*tiempo
+    print(f"la velocidad final es {velocidadFinal} metros por segundo")
+#Mediante la sentencia elif lo que hacemos es que en caso de que sea false el if el programa pregunte si la sentencia dentro del elif es True, y en caso de ser ejecutara lo que esta indexado 
+elif DatoACalcular=="altura": 
+    altura = ((tiempo**2)*9.81)/2
+    print(f"la altura es {altura} metros")
+#Con la sentencia else lo que hacemos es que ya como ultima opcion ejecute lo que tiene indexado el else
+#Como es la ultima opcion observar que no evalua si es false o true.
+else:
+    print("no se selecciono ninguna de las 2 opciones ejecute nuevamente")
+    
+    
