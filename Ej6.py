119ab81cb9218db96b4d1e0edacb057b6784c9f3
vss <maxi.valdes.v@gmail.com>
2024-04-29 19:19:52 -0400

    modificados:     Ej6.py

--- Ej6.py
+++ Ej6.py
@@ -1,8 +1,9 @@
 lote = int(input("Ingrese la cantidad de lote en kilogramos "))
+conteo = 0
 while lote>1:
-    print(lote)
+    print(f"la cantidad del lote es {lote} y van en el cuarteo numero {conteo}")
+    conteo+=1
     if lote/2>1:
         lote = lote/2
     else:
         break
-print(lote)
