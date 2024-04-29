30524b9f1eb344dac47dc0209116897cb5cfb2cd
vss <maxi.valdes.v@gmail.com>
2024-04-29 19:43:40 -0400

    modificados:     Ej7.py

--- Ej7.py
+++ Ej7.py
@@ -1,13 +1,13 @@
-Autorizado = input("Entregue rango de autorizacion \nbajo \nmedio \nalto \nOmitir con enter para no autorizar ")
+Autorizado = input("Entregue rango de autorizacion \nbaja \nmedia \nalta \nOmitir con enter para no autorizar ")
 print("\n")
 #Aca en caso de la variable de contener data es True y en caso de ser vacia es False
 if Autorizado: 
     print("Usted esta autorizado")
-    if Autorizado=="bajo":
+    if Autorizado=="baja":
         print("Tiene autorizacion de bajo nivel")
-    if Autorizado=="medio":
+    if Autorizado=="media":
         print("Tiene autorizacion de medio nivel")
-    if Autorizado=="alto":
+    if Autorizado=="alta":
         print("Tiene autorizacion de alto nivel")
 else:
     print("Usted no a sido autorizado con ningun rango")
