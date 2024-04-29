59b4cea86643659811662787355c09a8c7156e91
vss <maxi.valdes.v@gmail.com>
2024-04-29 19:22:55 -0400

    modificados:     Ej6.py

--- Ej6.py
+++ Ej6.py
@@ -3,7 +3,7 @@ conteo = 0
 while lote>1:
     print(f"la cantidad del lote es {lote} y van en el cuarteo numero {conteo}")
     conteo+=1
-    if lote/2>1:
+    if lote/2>=1:
         lote = lote/2
     else:
         break
