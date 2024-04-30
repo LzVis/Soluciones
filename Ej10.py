296116aebb69d7afe77767e2bf156ccf5b878296
vss <maxi.valdes.v@gmail.com>
2024-04-30 00:00:00 -0400

    modificados:     Ej10.py

--- Ej10.py
+++ Ej10.py
@@ -2,14 +2,14 @@ MetrosCuadrados = float(input("Introduzca cantidad de metros cuadrados del proye
 Costo = 1.7
 Profundidad = float(input("Introduzca la profundidas de la operacion "))
 
-if Profundidad>10:
+if Profundidad>=10:
     Costo = Costo *1.5
-elif Profundidad>20:
+elif Profundidad>=20:
     Costo = Costo *2
-elif Profundidad>30:
+elif Profundidad>=30:
     Costo = Costo *2.2
-elif Profundidad>40:
-    Costo = Costo *2.2
+elif Profundidad>=40:
+    Costo = Costo *2.5
 
 Presupuesto = Costo * MetrosCuadrados
 
